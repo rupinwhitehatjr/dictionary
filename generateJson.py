9a4411735bba7afa2ebbeb499b3b2443b0e08eb3
Rupin <rupin@whitehatjr.com>
2020-09-23 16:55:54 +0530

    firstfewwords

---
+++ generateJson.py
@@ -0,0 +1,62 @@
+from openpyxl import load_workbook
+import os
+from datetime import datetime
+import requests
+import json
+import re
+import uuid
+import pickle
+import os.path
+allowed=["dictionary"]
+
+def main():
+	xlFile=load_workbook("dictionary.xlsx")
+	
+	index=0
+	#fileURL="https://drive.google.com/file/d/1GPWteQTcT5xD-Riild6OT0o6BSwMlS3f/view?usp=sharing"
+	#getPublicURL(fileURL, google_drive_service)
+	for sheetname in xlFile.sheetnames:	
+		print(sheetname)	
+		if(sheetname in allowed):
+			generateJSON(xlFile, sheetname)
+				
+	
+	#xlFile.save("Championship Questions V2.xlsx")
+
+
+
+def generateJSON(fileref, shtname):
+	sheet = fileref[shtname]
+
+	
+	rowStart=2
+	rowEnd=30
+	#rows=122
+	offset=1
+	myDict={}
+	#firstWord=""
+	for row in range(rowStart,rowEnd):
+		word=sheet.cell(row, offset+0).value
+		word=word.lower()
+		definition= sheet.cell(row, offset+2).value  
+		blankObject={} 
+		if (word in myDict): 
+			blankObject=myDict[word]
+			#blankObject["definitions"]=[]
+			blankObject["definitions"].append(definition)
+			myDict[word] = blankObject
+		else: 
+			blankObject["definitions"]=[]
+			blankObject["definitions"].append(definition)
+			myDict[word] = blankObject
+
+	for key, value in myDict.items():
+		filename=key+".json"
+		with open(filename, "w") as jsonfile:
+			jsonfile.write(json.dumps(value))
+	
+	
+		
+
+
+main()
